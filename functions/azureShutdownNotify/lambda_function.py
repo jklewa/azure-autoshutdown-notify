6a3553794385a7790ea4dfa927db17c3a1cd1011
Jason Lewallen <jason.lewallen@returnpath.com>
2017-01-30 15:46:58 -0700

    Correct response format for API gateway

--- functions/azureShutdownNotify/lambda_function.py
+++ functions/azureShutdownNotify/lambda_function.py
@@ -9,6 +9,7 @@ sys.path.append(os.path.join(sys.path[0],'libs'))
 
 from pushbullet import Pushbullet
 import bitly_api
+import json
 
 pb = Pushbullet(os.environ['PUSHBULLET_API_KEY'])
 bitly = bitly_api.Connection(access_token=os.getenv('BITLY_ACCESS_TOKEN'))
@@ -24,15 +25,28 @@ Delay shutdown for 2 hours: {url120}
 """
 
 def lambda_handler(event, context):
-    print "Event:", event
-    event['url60'] = bitly.shorten(event['delayUrl60'])['url']
-    event['url120'] = bitly.shorten(event['delayUrl120'])['url']
-    event['urlSkip'] = bitly.shorten(event['skipUrl'])['url']
+    try:
+        if 'body' in event:
+            print "API Gateway event detected, converting event body"
+            event = json.loads(event['body'])
+        print "Event:", event
+        event['url60'] = bitly.shorten(event['delayUrl60'])['url']
+        event['url120'] = bitly.shorten(event['delayUrl120'])['url']
+        event['urlSkip'] = bitly.shorten(event['skipUrl'])['url']
 
-    title = title_template.format(**event)
-    url = url_template.format(**event)
-    body = body_template.format(**event)
-    
-    #push = pb.push_note(title, body=body)
-    push = pb.push_link(title, url, body=body)
-    return push
+        title = title_template.format(**event)
+        url = url_template.format(**event)
+        body = body_template.format(**event)
+
+        #push = pb.push_note(title, body=body)
+        push = pb.push_link(title, url, body=body)
+        return {
+            'statusCode': 200,
+            #'body': json.dumps(push, sort_keys=True, indent=2, separators=(',', ':')),
+            'body': json.dumps({'success':True})
+        }
+    except Exception, e:
+        return {
+            'statusCode': 400,
+            'exception': e,
+        }
