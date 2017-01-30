9fcf15587211b549d33e8c1d265064b4814586e0
Jason Lewallen <jason.lewallen@returnpath.com>
2017-01-30 15:16:27 -0700

    cleanup and bugfixes

--- functions/azureShutdownNotify/lambda_function.py
+++ functions/azureShutdownNotify/lambda_function.py
@@ -4,13 +4,14 @@ A Lambda function to send a pushbullet notification to an Azure VM before its
 automatically shuts down daily
 """
 
-from __future__ import print_function
-from libs.pushbullet import Pushbullet
-import libs.bitly_api
-import os
+import os, sys
+sys.path.append(os.path.join(sys.path[0],'libs'))
+
+from pushbullet import Pushbullet
+import bitly_api
 
 pb = Pushbullet(os.environ['PUSHBULLET_API_KEY'])
-bitly = bitly_api.Connection(access_token=os.getenv(BITLY_ACCESS_TOKEN))
+bitly = bitly_api.Connection(access_token=os.getenv('BITLY_ACCESS_TOKEN'))
 
 url_template = "{url60}"
 title_template = "{vmName} will be shutting down soon..."
@@ -23,9 +24,15 @@ Delay shutdown for 2 hours: {url120}
 """
 
 def lambda_handler(event, context):
-    event['url60'] = bitly.shorten(event['delayUrl60'])
-    event['url120'] = bitly.shorten(event['delayUrl120'])
-    event['urlSkip'] = bitly.shorten(event['skipUrl'])
+    print "Event:", event
+    event['url60'] = bitly.shorten(event['delayUrl60'])['url']
+    event['url120'] = bitly.shorten(event['delayUrl120'])['url']
+    event['urlSkip'] = bitly.shorten(event['skipUrl'])['url']
 
-    push = pb.push_link(title_template.format(event), url_template.format(event), body=body_template.format(event))
+    title = title_template.format(**event)
+    url = url_template.format(**event)
+    body = body_template.format(**event)
+    
+    #push = pb.push_note(title, body=body)
+    push = pb.push_link(title, url, body=body)
     return push
